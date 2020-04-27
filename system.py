a046fbfc41b5783ed2bc447e6532e2a4dacde323
donadivarun <donadivarun@gmail.com>
2020-04-27 05:35:20 -0700

    Update system.py
    Fixed some errors

--- system.py
+++ system.py
@@ -23,7 +23,7 @@ class System:
         
     def remove_pedastrian(self, pedastrian):
         #remove a pedastrian from the grid 
-        if self.pedastrian has pedastrian:
+        if pedastrian in self.pedastrian:
             self.grid[pedastrian[0]][pedastrian[1]].state = 0
             self.pedastrian.remove(pedastrian)
         else:
@@ -31,26 +31,30 @@ class System:
         
     def add_target(self, target):
         #set the target of the grid, limit of 1 target
-        if self.target is not none:
+        if self.target is not None:
             self.grid[self.target[0]][self.target[1]].state = 0
         self.target = target
         self.grid[target[0]][target[1]].state = 2
         
-    def remove_target(self, target = self.target):
+    def remove_target(self, target = None):
         #remove the target from the grid
-        if self.target == target:
-            self.target = None
+        if target is not None:
+            if self.target == target:
+                self.target = None
+            else:
+                print("No such target found!")
         else:
-            print("No such target found!")
+            self.target = None
         
     def add_obstacle(self, obs):
         #add obstacle in the grid
         self.grid[obs[0]][obs[1]].state = 3
         self.obstacle.append(obs)
         
-    def euclidean_distance(x, y):
-        #distance between a tuple of two coordinates
-        return math.sqrt((x[0]-y[0])**2 + (x[1]-y[1])**2)
+def euclidean_distance(x, y):
+    #this one has some problems
+    #distance between a tuple of two coordinates
+    return math.sqrt((x[0]-y[0])**2 + (x[1]-y[1])**2)
     
     
 class Cell:
